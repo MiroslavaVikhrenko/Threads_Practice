8a783307798877a60f3ba1cce6f0e2653991f777
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2025-05-06 14:39:07 -0600

    Add project files.

---
+++ 20250506_Task1/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _20250506_Task1
+{
+    //Написать программу, в которой при запуске начинают одновременно работать 3 метода,
+    //выводящие на экран числа в интервале от n  до z, с интервалом на вывод – полсекунды.
+    //Значения n и z, передаются в метод.
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread t1 = new Thread(() => PrintNumbers(1, 5));
+            t1.Start();
+            t1.Join();
+            Thread t2 = new Thread(() => PrintNumbers(6, 10));
+            t2.Start();
+            t2.Join();
+            Thread t3 = new Thread(() => PrintNumbers(11, 15));
+            t3.Start();
+            t3.Join();
+
+        }
+
+        static void PrintNumbers(int n, int z)
+        {
+            for (int i = n; i <= z; i++)
+            {
+                Console.WriteLine($"{i}");
+                Thread.Sleep(500);
+            }
+        }
+    }
+}
---
+++ Threads_Practice/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Threads_Practice
+{
+    // Создать метод, выводящий на консоль каждую секунду, текущее время, в отдельном потоке.
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread t = new Thread(Method);
+            t.Start();
+            Console.ReadKey();
+        }
+
+        static void Method()
+        {
+            while (true)
+            {
+                Console.WriteLine($"Current time: {DateTime.Now}");
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
